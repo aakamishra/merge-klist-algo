82da8b591450a6dcaef53c320858e5c13d5753b2
Aakash Mishra <amishra@rexhomes.com>
2022-09-12 00:32:05 -0400

    Add File for Algorithm Code and Generic Classes
    The commit adds the assignment prompt in a file docustring.
    I also added the solution class framework for the problem.
    This also adds the definition of a linked list node.

---
+++ merge-k-lists.py
@@ -0,0 +1,35 @@
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
