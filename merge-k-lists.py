f855c8783eb16e1dce682eb6a0c18ef036dd4116
Aakash Mishra <amishra@rexhomes.com>
2022-09-12 14:27:03 -0400

    Add MergeKListsTest Class
    Class implements a testing framework to convert input and
    output into testable and assertable statements while also timing methods
    in order to mantain benchmarks. Prevents program from crashing if an
    error is encountered. Enters a soft failure with try and except.

--- merge-k-lists.py
+++ merge-k-lists.py
@@ -16,8 +16,14 @@ merging them into one sorted list:
 
 """
 
+import time
+
 
 class ListNode(object):
+    """
+    ListNode class for implementing linked lists
+    """
+
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
@@ -39,7 +45,7 @@ class Solution(object):
         n = len(final_list)
         return_ptr = new_head = ListNode(val=-1)
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
                 if final_list[i] > final_list[j]:
                     temp = final_list[i]
                     final_list[i] = final_list[j]
@@ -72,5 +78,62 @@ class Solution(object):
         return_ptr = combined_list = ListNode(val=-1)
         for ls in lists:
             combine_helper(combined_list, ls)
-       
+
         return return_ptr.next
+
+
+class MergeKListsTest:
+    """
+    Class for testing solution code from the solution class
+    with given test parameters for a LinkedList
+    """
+
+    def __init__(
+            self,
+            expected_input,
+            expected_output,
+            solution_method) -> None:
+        self.expected_input = expected_input
+        self.expected_output = expected_output
+        self.method = solution_method
+
+    def list_parser(self, current: ListNode) -> list:
+        final_list = []
+        while current:
+            final_list.append(current.val)
+            current = current.next
+
+        return final_list
+
+    def input_generator(self):
+        curr = return_ptr = ListNode(val=-1)
+        list_of_lists = []
+        for input_list in self.expected_input:
+            curr = return_ptr = ListNode(val=-1)
+            for val in input_list:
+                curr.next = ListNode(val=val)
+                curr = curr.next
+            list_of_lists.append(return_ptr.next)
+
+        return list_of_lists
+
+    def check_output(self, output):
+        assert self.list_parser(output) == self.expected_output
+
+    def run_test_case(self):
+        print("Running Test Case")
+        input = self.input_generator()
+        print("Test Input: ", self.expected_input)
+        start = time.time()
+        test_output = self.method(input)
+        end = time.time()
+        print("Recieved Output: ", self.list_parser(test_output))
+        print("Expected Output: ", self.expected_output)
+        try:
+            self.check_output(test_output)
+            print("Test Case Passed!")
+        except AssertionError as e:
+            print("Test Case Failed: ", e)
+
+        print("Test Time Elapsed: ", end - start, " seconds")
+        print("----------------------------")
